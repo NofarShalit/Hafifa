2d66e08f258d2cbbdda773a90eef4894e45750a7
p <sydneyfirehydrant@gmail.com>
2022-10-30 21:26:31 +0200

    asd

--- Training/Or/Answers/Process Memory/Process Memory Q4.py
+++ Training/Or/Answers/Process Memory/Process Memory Q4.py
@@ -1,3 +1,20 @@
+from multiprocessing import active_children, Process
+from time import sleep
+
+def get_process_by_name(pname):
+    processes = active_children()
+    for process in processes:
+        if process.name == pname:
+            return process
+    return None
+
+def main():
+    print(get_process_by_name("Everything.exe"))
+
+if __name__ == "__main__":
+    main()
+
+"""
 import subprocess as sub
 
 def get_pid(name):
@@ -8,3 +25,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+"""
