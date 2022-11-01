5993e4d24722244870a37b19188bcacd4f16183e
pgriffinski <116191297+pgriffinski@users.noreply.github.com>
2022-11-01 13:35:36 -0700

    sd

--- Training/Or/Answers/Process Memory/Process Memory Q4.py
+++ Training/Or/Answers/Process Memory/Process Memory Q4.py
@@ -1,11 +1,13 @@
 import psutil
 
-PROCNAME = "Everything.exe"
+PROCNAME = "dwm.exe"
 
 def main():
     for proc in psutil.process_iter():
-        if proc.name().lower() == PROCNAME:
-            print(proc)
+        if proc.name() == PROCNAME:
+            for pproc in psutil.process_iter():
+                if proc.ppid() == pproc.pid:
+                    print(pproc.name())
 
 if __name__ == "__main__":
     main()
