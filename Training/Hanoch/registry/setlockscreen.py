3ab5288dc45eac539847aeaa6681be332e577bc0
pgriffinski <116191297+pgriffinski@users.noreply.github.com>
2022-11-01 01:25:15 -0700

    answers

--- Training/Hanoch/registry/setlockscreen.py
+++ Training/Hanoch/registry/setlockscreen.py
@@ -2,13 +2,17 @@ import winreg
 
 
 ROOT = winreg.HKEY_LOCAL_MACHINE
-SUB = r'SOFTWARE\Policies\Microsoft\Windows\Personalization'
+SUB = r'SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP'
 
 
 def main():
+    
+    # C:\Users\moshiko\Downloads\1200px-Coat_of_arms_of_Ramat_Gan.svg.png
     path = input('Enter lockscreen image path')
     key = winreg.CreateKeyEx(ROOT, SUB)
-    winreg.SetValueEx(key, 'LockScreenImage', 0, winreg.REG_SZ, path)
+    winreg.SetValueEx(key, 'LockScreenImagePath', 0, winreg.REG_SZ, path)
+    winreg.SetValueEx(key, 'LockScreenImageUrl', 0, winreg.REG_SZ, path)
+    winreg.SetValueEx(key, 'LockScreenImageStatus', 0, winreg.REG_DWORD, 1)
     winreg.CloseKey(key)
 
     
