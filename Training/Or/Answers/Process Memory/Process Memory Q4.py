3178a44489f15c902f2f7d8fa96dc766ed3d778d
= <=>
2022-11-01 22:18:24 +0200

    Update Process Memory Q4.py

--- Training/Or/Answers/Process Memory/Process Memory Q4.py
+++ Training/Or/Answers/Process Memory/Process Memory Q4.py
@@ -4,38 +4,8 @@ PROCNAME = "Everything.exe"
 
 def main():
     for proc in psutil.process_iter():
-        if proc.name() == PROCNAME:
+        if proc.name().lower() == PROCNAME:
             print(proc)
 
 if __name__ == "__main__":
     main()
-
-"""
-from multiprocessing import active_children, Process
-from time import sleep
-
-def get_process_by_name(pname):
-    processes = active_children()
-    for process in processes:
-        if process.name == pname:
-            return process
-    return None
-
-def main():
-    print(get_process_by_name("Everything.exe"))
-
-if __name__ == "__main__":
-    main()
-"""
-"""
-import subprocess as sub
-
-def get_pid(name):
-    return sub.check_output(["pidof ",name])
-
-def main():
-    get_pid("Everything.exe")
-
-if __name__ == "__main__":
-    main()
-"""
