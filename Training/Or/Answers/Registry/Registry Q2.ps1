d0b21238f5fe2483d97237599dcd4acc084d15f3
= <=>
2022-11-02 11:42:15 +0200

    Update Registry Q2.ps1

--- Training/Or/Answers/Registry/Registry Q2.ps1
+++ Training/Or/Answers/Registry/Registry Q2.ps1
@@ -3,4 +3,4 @@ $VALUE_NAME = "LockScreenImage"
 $VALUE = Read-Host -Prompt "Enter the image path"
 if (-Not (Test-Path "$REGISTRY_PATH\Personalization"))
 { New-Item -Path $REGISTRY_PATH -Name "Personalization" }
-New-ItemProperty -Path "$REGISTRY_PATH\Personalization" -Name $VALUE_NAME -Value $VALUE -PropertyType "REG_SZ"
+New-ItemProperty -Path "$REGISTRY_PATH\Personalization" -Name $VALUE_NAME -Value $VALUE -PropertyType "String" -Force
