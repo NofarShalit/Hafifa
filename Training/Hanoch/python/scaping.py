c81413a7dfbf83d6ce4a1efa60866ee3947e6970
pgriffinski <116191297+pgriffinski@users.noreply.github.com>
2022-11-02 12:57:52 -0700

    answers

--- Training/Hanoch/python/scaping.py
+++ Training/Hanoch/python/scaping.py
@@ -3,10 +3,14 @@ from scapy.all import *
 
 def main():
     
+    # Send data through ICMP- ICMP tunnel
+    p = sr1(IP(dst="1.1.1.1")/ICMP()/"data")
+    
     # Sniff HTTP packets
     pcap = sniff(filter='icmp', count=100)
     pcap.summary()
     wrpcap("mycap.pcap", pcap)
+
 
     
 if __name__ == '__main__':
