d494a3f6bfb862141abc0e5047288ce3845c5f44
= <=>
2022-11-02 10:06:50 +0200

    sdf

--- Training/Or/Answers/Registry/Registry Q1.c
+++ Training/Or/Answers/Registry/Registry Q1.c
@@ -6,7 +6,7 @@
 int main()
 {
     HKEY ROOT_KEY = "HKEY_LOCAL_MACHINE";
-    LPCSTR SUB_KEY = "SOFTWARE\Microsoft\Windows NT\CurrentVersion\DiskDiagnostics";
+    LPCSTR SUB_KEY = "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\DiskDiagnostics";
     HKEY OPEN_KEY;
     PFILETIME LAST_WRITE_TIME;
     RegOpenKeyExW(ROOT_KEY, SUB_KEY, 0, KEY_ALL_ACCESS, &OPEN_KEY);
