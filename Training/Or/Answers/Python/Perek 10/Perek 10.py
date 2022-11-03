1190a97845d195d71dd700ca10d68e164d3e9354
= <=>
2022-11-03 09:32:01 +0200

    Update Perek 10.py

--- Training/Or/Answers/Python/Perek 10/Perek 10.py
+++ Training/Or/Answers/Python/Perek 10/Perek 10.py
@@ -1,5 +1,5 @@
 class Dog:
-    def __init__(self, name, age):
+    def __init__(self, name="deault name", age=0):
         self.__name = name
         self.__age = age
 
@@ -8,6 +8,16 @@ class Dog:
 
     def get_age(self):
         return self.__age
+    def get_name(self):
+        return self.__name
+    
+    def set_name(self, name):
+        self.__name = name
+    def set_age(self, age):
+        self.__age = age
+    
+    def __str__(self):
+        print(f"The animal is {self.get_age()} and named {self.get_name()}")
 
 def main():
     kermit = Dog("kermit", 13)
